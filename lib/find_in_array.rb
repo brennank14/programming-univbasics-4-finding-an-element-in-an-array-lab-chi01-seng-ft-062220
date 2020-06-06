8eea97dc97865f91e85ce15a4bb04fb972fbb877
Kathleen Brennan <brennank14@gmail.com>
2020-06-06 03:45:13 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,10 +1,3 @@
-scale = [2,3,4,5]
-scale_2 = [6,5,4,3]
-hill = [1,2,3,4,5,4,3,2,1]
-valley = [5,4,3,2,1,0,1,2,3,4,5]
-zig_zag = [500,4,1000,5,250]
-
-
 def find_element_index(array, value_to_find)
   count = 0
   while count < array.length do
@@ -15,5 +8,3 @@ def find_element_index(array, value_to_find)
     end
   end
 end
-
-puts find_element_index(scale, 4)
