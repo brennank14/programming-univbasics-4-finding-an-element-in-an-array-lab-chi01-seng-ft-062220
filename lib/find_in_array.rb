ab94daad4f2c53ea9a74f1819fee384dcaac5c93
Kathleen Brennan <brennank14@gmail.com>
2020-06-06 03:37:30 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   count = 0
   while array[count] < array.length
     if array[count] == value_to_find
-      puts count
+      return count
     else
       count += 1
     end
